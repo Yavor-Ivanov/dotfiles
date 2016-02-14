bb25ccdbf2f0c02baa30ac4000f2d424194cafa6
Yavor Ivanov <ppm@ppm.local>
2016-02-14 13:06:06 +0200

    Add: Packages.

--- main.sh
+++ main.sh
@@ -5,5 +5,6 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 . "$DIR/defaults.sh"
 . "$DIR/remote_session_aliases.sh"
+. "$DIR/shortcut_aliases.sh"
 
 eval $default_session
---
+++ shortcut_aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+alias clip="nc localhost 8377"
+alias e="vim"
+alias g="grep"
