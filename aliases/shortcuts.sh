4fc3c24c7c03e89e2367c72cb7e75d56c2617eaa
Yavor Ivanov <ppm@ppm.devlabs>
2016-02-15 16:06:44 +0200

    Added: Shortcuts.

--- aliases/shortcuts.sh
+++ aliases/shortcuts.sh
@@ -2,7 +2,10 @@
 
 alias clip="nc 127.0.0.1 8377 -q0"
 alias e="vim"
-alias g="grep"
+alias g="grep -E"
 alias la="ls -la"
 alias up="cd .."
 alias cd..="up"
+alias py="python"
+alias py3="python3"
+alias p3="py3"
