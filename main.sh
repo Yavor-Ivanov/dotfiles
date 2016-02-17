d373f23ac13530db4500768ac75ea36765e9d958
Yavor Ivanov <ppm@ppm.devlabs>
2016-02-17 11:14:39 +0200

    Add: Local configs.
    I've separated the recursive execution of commands into a `recursive`
    function. I'll need to refarctor this later, so that I don't have two
    commands for the same action.

--- main.sh
+++ main.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+# Resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ];
+do
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	SOURCE="$(readlink "$SOURCE")"
-	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+
+	# if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 shopt -s expand_aliases
 
-function include()
+function recursive()
 {
-	local incl_path=$1
-	local absolute=$2
+	local cmd=$1
+	local incl_path=$2
+	local absolute=$3
 	shift
 	shopt -s expand_aliases
 	if [[ -z $absolute ]]
@@ -22,12 +27,21 @@ function include()
 
 	while read f
 	do
-		source "$f" "$@"
+		$($cmd "$f")
 	done <<< "$(echo $incl_path | tr " " "\n")"
 	return 0
 }
 
-include "defaults.sh"
-include "aliases/*"
+function include()
+{
+	source "$1" "$@"
+}
 
-eval $default_session
+function invoke()
+{
+	. ./"$1"
+}
+
+include "defaults.sh"
+recursive include "aliases/*"
+invoke "local/main.sh"
