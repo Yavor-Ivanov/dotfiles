612d2e201aec3060126f0f9f0740d4f9004ba25d
Yavor Ivanov <yavor@devlabs.bg>
2016-05-19 12:05:02 +0300

    Add: PS1 prompt format.

--- defaults.sh
+++ defaults.sh
@@ -8,3 +8,15 @@ export EDITOR=$VISUAL
 default_host=juicebox
 default_client=mosh
 default_user=ppm
+
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+case "$TERM" in
+xterm*|rxvt*|*color)
+	PS1="\u@\h:\w\\$ \[$(tput sgr0)\]"
+    ;;
+*)
+    ;;
+esac
