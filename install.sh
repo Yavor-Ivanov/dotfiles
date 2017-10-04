deafb4c8aebb1a06363437e5eb3f61edf0b70e1d
Yavor Ivanov <yavor@devlabs.bg>
2017-10-04 20:07:53 +0300

    Add: W3m settings.

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 ln -s "$DIR/.vimrc" ~/.vimrc
 ln -s "$DIR/.vim" ~/.vim
 ln -s "$DIR/.tmux.conf" ~/.tmux.conf
+ln -s "$DIR/.w3m" ~/.w3m
 sudo ln -s "$DIR/bin/" /usr/local/bin/
