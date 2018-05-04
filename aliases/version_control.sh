8093ebb21adb231f38a0c275a50e2bbbbaa77852
Yavor Ivanov <yavor@devlabs.bg>
2018-05-04 13:40:53 +0300

    Add: Version control alias to merge latest version of branch_name in current branch.

--- aliases/version_control.sh
+++ aliases/version_control.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+get_first_match() {
+	# @Todo <Yavor>: Fix this to list all matches and autocomplete somehow.
+	# @Todo <Yavor>: Make this accept strings with spaces and perform multiple greps.
+	# @Bug <Yavor>: This doesn't pick the shortest match!
+	first_match="$(git branch | grep "${cmd_args[0]}" | sed 's/\* \(.*\)/\1/' | sed 's/ *\(.*\)/\1/' | head -1)"
+	echo "$first_match"
+}
+
 version_control() {
 	script_args=( $@ )
 	arg_len=${#script_args[@]}
@@ -14,9 +22,7 @@ version_control() {
 			git pull
 		;;
 		switch)
-			# @Todo <Yavor>: Fix this to list all matches and autocomplete somehow.
-			first_match="$(git branch | grep "${cmd_args[0]}" | sed 's/\* \(.*\)/\1/' | sed 's/ *\(.*\)/\1/' | head -1)"
-			git checkout $first_match
+			git checkout "$(get_first_match "${cmd_args[0]}")"
 		;;
 		status)
 			git status
@@ -36,6 +42,20 @@ version_control() {
 		diff)
 			git diff $cmd_args
 		;;
+		update-merge)
+			has_uncommited_files="$(git status -s)"
+			if [ -n "$has_uncommited_files" ]; then
+				git stash
+			fi
+			branch_name="$(get_first_match ${cmd_args[0]})"
+			git checkout "$branch_name"
+			git pull
+			git checkout @{-1}
+			git merge "$branch_name"
+			if [ -n "$has_uncommited_files" ]; then
+				git stash pop
+			fi
+		;;
 		*)
 			echo "ERROR: '$cmd' is not implemented!"
 		;;
@@ -45,6 +65,7 @@ va() { version_control add "$@"; }
 vc() { version_control commit "$@"; }
 vw() { version_control switch "$@"; }
 vu() { version_control update "$@"; }
+vum() { version_control update-merge "$@"; }
 vp() { version_control push "$@"; }
 vs() { version_control status "$@"; }
 vl() { version_control log "$@"; }
